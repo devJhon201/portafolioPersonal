79150404f7e758ab78797f71502327a71e0453b8
Jhon C <82723270+devJhon201@users.noreply.github.com>
2021-06-03 19:42:31 +0200

    entrada en movil arreglada, cambio en immagenes en movil comenzado

--- recursos/js/scripts.js
+++ recursos/js/scripts.js
@@ -5,7 +5,18 @@ window.addEventListener("DOMContentLoaded", (event) => {
 		/* The viewport is less than, or equal to, 700 pixels wide */
 		const headerStyle = document.getElementById("entrada-pagina-principal");
 		headerStyle.style.height = window.innerHeight + "px";
-	} else {
-		/* The viewport is greater than 768 pixels wide */
+		let fotoProyecto = document.getElementById("camila-img");
+		fotoProyecto.setAttribute("src", "./recursos/img/camila-movil.jpeg");
+		fotoProyecto = document.getElementById("excursion-img");
+		fotoProyecto.setAttribute("src", "./recursos/img/excursion-movil.jpeg");
+		fotoProyecto = document.getElementById("fotomat-img");
+		fotoProyecto.setAttribute("src", "./recursos/img/fotomat-movil.jpeg");
+		fotoProyecto = document.getElementById("tea-img");
+		fotoProyecto.setAttribute("src", "./recursos/img/tea-movil.jpeg");
+		fotoProyecto = document.getElementById("style-img");
+		fotoProyecto.setAttribute("src", "./recursos/img/style-movil.jpeg");
 	}
+	//  else {
+	// 	/* The viewport is greater than 768 pixels wide */
+	// }
 });
